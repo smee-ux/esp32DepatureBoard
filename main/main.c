4d13cfebff8d18e055a78fa9f8e3c35b5abb6352
axel <axel@>
2026-08-14 11:37:15 +0200

    Bitmap only used for train image Now uses a textbox to display minute counter

--- main/main.c
+++ main/main.c
@@ -14,17 +14,7 @@
 #include "soc/gpio_num.h"
 #include "ssd1306.h"
 #include "font8x8_basic.h"
-#include "bitmaps/number_bitmaps.c"
-
-/*
- You have to set this config value with menuconfig
- CONFIG_INTERFACE
-
- for SPI
- CONFIG_CS_GPIO
- CONFIG_DC_GPIO
- CONFIG_RESET_GPIO
-*/
+#include "bitmaps/train_bitmap.c"
 
 #define tag "SSD1306"
 
@@ -35,19 +25,24 @@ void send_queue(void *arg){
 	while(1){
 		for (int num = 10; num > 0; num--) {
 			xQueueSend(queue, (void *)&num, pdMS_TO_TICKS(0));
-			vTaskDelay(pdMS_TO_TICKS(500));
+			vTaskDelay(pdMS_TO_TICKS(1000));
 		}
 	}
 }
 
 void recieve_queue(void *arg){
 	int a;
+	char* line = "C ";
 	
 	while(1){
 		if (xQueueReceive(queue, (void *)&a, pdMS_TO_TICKS(100))) { // den skal have en adresse den kan skrive i
-			// ssd1306_clear_screen(&device, false);
 			ESP_LOGI("switch", "number recieved: %d", a);
-		}
+			
+			// convert recieved number to a string that can be passed to ssd1306
+			char departure_string[10];
+			snprintf(departure_string, 10, "%s:%d min", line,a);			
+			ssd1306_display_text_box1(&device, 3, 48, departure_string, 10, 10, false, 0);
+		}	
 	}
 }
 
@@ -57,7 +52,7 @@ void app_main(void)
 	ssd1306_init(&device, 128, 64);
 	ssd1306_clear_screen(&device, false);
 	ssd1306_contrast(&device, 0xFF);
-	ssd1306_bitmaps(&device, 0, 0, image_2, 64, 64, false);
+	ssd1306_bitmaps(&device, 0, 0, image_1, 128, 64, false);
 
 	queue = xQueueCreate(5, sizeof(int));
 	xTaskCreate(send_queue, "send_queue", 4096, NULL, 10, NULL);
